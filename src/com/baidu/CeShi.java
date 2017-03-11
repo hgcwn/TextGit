26abae1e66aeb60f9b4275533bc4aff1232492c8
王宁 <2787861683@qq.com>
2017-03-11 09:53:57 +0800

    第四次了吧

--- src/com/baidu/CeShi.java
+++ src/com/baidu/CeShi.java
@@ -5,5 +5,9 @@ public class CeShi {
 	public static void main(String[] args) {
 		System.out.println("GitHub");
 		System.out.println("第二次提交了");
+		//缓存和服务器不一样就有小箭头出现
+		//add之后一样了出现*
+		//commit之后就完成了
+		System.out.println("第三次提交了");
 	}
 }
